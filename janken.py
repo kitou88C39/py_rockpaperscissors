8dee3d703a8a35c3c7e2f48537ff9abadc27e734
kitou88C39 <kanfutrooper@icloud.com>
2024-01-11 15:26:42 +0900

    Create a rock-paper-scissors game that can be settled with commands

--- janken.py
+++ janken.py
@@ -1,12 +1,12 @@
 gu, choki, pa = "グー","チョキ","パー"
 win, draw, lose = "勝","相子","負"
 
-my_hand = go
+my_hand = gu
 enemy_hand = pa
 
 if my_hand == enemy_hand:
     print(draw)
-elif my_hand == gu and enemy_hand == choki:
+elif (my_hand == gu and enemy_hand == choki) or:
     print(win)
 else
     print(lose)
