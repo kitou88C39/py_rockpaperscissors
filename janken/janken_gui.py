139382b7c0c7d1c19864d558786c9e02d28de05b
kitou88C39 <kanfutrooper@icloud.com>
2024-01-12 15:29:47 +0900

    Incorporate rock-paper-scissors processing

--- janken/janken_gui.py
+++ janken/janken_gui.py
@@ -1,6 +1,14 @@
 import tkinter as tk
 from PIL import Image, ImageTk //PIL パッケージから Image クラスと ImageTk クラスをインポート
 
+hands = ["グー", "チョキ", "パー"]
+win, draw, lose = "勝","相子","負"
+rules = {
+    (0,0):draw,(0,1):win, (0,2):lose,
+    (1,0):lose,(1,1):draw, (1,2):win,
+    (2,0):win,(2,1):lose, (2,2):draw
+}
+
 //tkinterを使って画面を構築
 root = tk.Tk()
 root.geometry("420x400")
