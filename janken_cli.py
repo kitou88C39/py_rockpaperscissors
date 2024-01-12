07dcb5e045f1357a8d9157b7cffee3127868fd93
kitou88C39 <kanfutrooper@icloud.com>
2024-01-12 13:30:28 +0900

    Write a Rock Paper Scissors program on the command line

--- janken_cli.py
+++ janken_cli.py
@@ -8,16 +8,22 @@ rules = {
     (2,0):win,(2,1):lose, (2,2):draw
 }
 
-while True:
+while True://無限ループ
     //inputを受け取って、intに変換する
-    my_hand = int(input('''0~2の数字を入力して下さい 0:グー, 1:チョキ, 2:パー >>>'''))
+    my_hand = int(input(
+    '''0~2の数字を入力して下さい //''' '''　3連クォート　
+    0:グー, 1:チョキ, 2:パー
+    >>>'''))
     enemy_hand = ramdom.randint(0, 2)
 
+    //自分の手と相手の手を表示する
     my_hand_str = hands[my_hand]
     enemy_hand_str = hands[enemy_hand]
     print("自分:" + "my_hand_str" + "相手:" + enemy_hand_str)
 
+    //相子だったら、もう一度やり直す
     result = rules[(my_hand, enemy_hand)]
     print(result)
+    //相子でなければ、終了
     if enemy_hand != my_hand:
         break
