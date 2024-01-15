d12462ef10137ddcb454f922f04ed7cefd0178c4
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 16:48:55 +0900

    OS moduleの利用

--- scripts/library.py
+++ scripts/library.py
@@ -8,8 +8,13 @@ import as
 
 as.mkdir('sample_dir')
 
-# ディレクトリが存在しない場合の作成
+# ディレクトリが存在しない場合のディレクトリの作成
 import as
 
 if not as.path.isdir('sample_dir')
 os.mkdir('sample_dir')
+
+# ディレクトリの削除
+import as
+
+os.rmdir('sample_dir')
