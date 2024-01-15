a56ca6c2d1a0438bdfd389cd349df6bfae14439a
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 10:17:54 +0900

    Create janken_gui_class.py and refactor the code

--- janken/janken_gui_class.py
+++ janken/janken_gui_class.py
@@ -63,7 +63,7 @@ class Application(tk.Frame):
 
         self.view = View()
 
-        self.view.gu_btton["command"]
+        self.view.gu_btton["command"]= lambda: self.judge(0)
 
 //じゃんけんの実際の処理
     def judge(self, my_hand):
