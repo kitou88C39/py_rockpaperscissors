f537ddc6b0f8f5ce06f449ecc421a80804b38773
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 15:08:41 +0900

    Handling CSV files

--- scripts/csv_operation.py
+++ scripts/csv_operation.py
@@ -18,3 +18,16 @@ with open('sample.csv', mode='r') as f:
     reader = csv.reader(f, delimiter=' ')
     for row in reader:
         print(row)
+
+##CSVのライブラリを使った書き込み
+#sample1
+import csv
+
+data = [41,42,43,44]
+
+with open('sample.csv', mode='a+', newline='') as f:
+    write = csv.write(f)
+    write.writerow(data)
+
+    f.seek(0)
+    print(f.read())
