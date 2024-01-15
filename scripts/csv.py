2d98e225fb81b2b0dd72247c7ae830001eeb4900
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 14:43:12 +0900

    Handling CSV files

--- scripts/csv.py
+++ scripts/csv.py
@@ -1,9 +1,6 @@
 import csv
 
-data = '''
-11,12,13,14
-21,22,23,24
-'''
-
-with open("sample.csv",mode="w") as f:
-    f.write(data)
+with open('sample.csv', mode='f') as f:
+    reader = csv.reader(f)
+    for row in reader:
+        print(row)
