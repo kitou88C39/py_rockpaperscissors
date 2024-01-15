50422c422393eaf7444367f58389a219b4bdcd47
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 17:01:55 +0900

    OS moduleの利用

--- scripts/library.py
+++ scripts/library.py
@@ -46,3 +46,19 @@ p.touch()
 
 print(p)
 print(type(p))
+
+#sample3(using open)
+import as
+import pathlib
+
+os.makedirs('sample_dir/sample_dir2', exist_ok=True)
+
+p = pathlib.Path('sample_dir/sample.txt')
+p.touch()
+
+with p.open(mode='w') as f:
+    f.write('Good Morining')
+
+with p.open(mode='w') as f:
+    print(f.read())
+
