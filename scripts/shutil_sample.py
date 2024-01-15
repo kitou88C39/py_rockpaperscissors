a067f22ce88c4e52f0a833ab4b03ed5b81eb5994
kitou88C39 <kanfutrooper@icloud.com>
2024-01-15 17:48:27 +0900

    glob moduleの利用

--- scripts/shutil_sample.py
+++ scripts/shutil_sample.py
@@ -2,3 +2,10 @@
 import shutil
 
 shutil.copy('sample_dir1/aaa.txt', 'sample_dir1/sample_dir2/xxx.txt')
+
+#sample2
+import glob
+import shutil
+
+shutil.copy('sample_dir1/sample_dir2', 'sample_dir1/sample_dir3')
+print(glob.glob('sample_dir1/sample_dir3/*'))
