0e66a9eb6cdc35a3fb8cb41f7787ca7cb7b7a9a3
kitou88C39 <kanfutrooper@icloud.com>
2024-01-16 13:06:55 +0900

    例外処理とtry文

--- error_sample.py
+++ error_sample.py
@@ -46,3 +46,17 @@ except ZeroDivisionError as e:
     print(type(e))
 
 print('Finish')
+
+# sample5
+um = [0, 1, 2]
+
+try:
+    a = 1 / 0
+    print(a)
+    print(nume[3])
+except ZeroDivisionError, IndexError as e:
+    print(e)
+    print(type(e))
+    print('要素が3つ以下です')
+
+print('Finish')
