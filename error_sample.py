835d01bd7885c01e9c6c15e4d5a096160c7a5c36
kitou88C39 <kanfutrooper@icloud.com>
2024-01-16 11:59:39 +0900

    例外処理とtry文

--- error_sample.py
+++ error_sample.py
@@ -1,8 +1,21 @@
+# sample1
 num = [0, 1, 2]
 
 try:
     print(nume[3])
 except IndexError:
+    print('要素が3つ以下です')
+
+print('Finish')
+
+# sample2
+num = [0, 1, 2]
+
+try:
+    print(nume[3])
+except IndexError as e:
+    print(e)
+    print(type(e))
     print('要素が3つ以下です')
 
 print('Finish')
