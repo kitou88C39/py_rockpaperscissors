b8d76ba6db0cd2ca0df3ed25f5ae9957456160c5
kitou88C39 <kanfutrooper@icloud.com>
2024-01-17 14:17:15 +0900

    属性の参照や代入が失敗した場合のエラー

--- debug/error.py
+++ debug/error.py
@@ -4,3 +4,11 @@
 
 a = 12
 a.replace(1, 2)
+
+---------------------------------------------------------------------------
+AttributeError                            Traceback (most recent call last)
+Input In [1], in <cell line: 2>()
+      1 a = 12
+----> 2 a.replace(1, 2)
+
+AttributeError: 'int' object has no attribute 'replace'
